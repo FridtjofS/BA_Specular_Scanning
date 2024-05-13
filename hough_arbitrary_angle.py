3b68db01c7494d1a2e90d055de5f99a4f22f7ff5
FridtjofS <93829965+FridtjofS@users.noreply.github.com>
2024-05-13 07:27:23 +0200

    ICP Iterative Clostes Point (modified) hough space matching

--- hough_arbitrary_angle.py
+++ hough_arbitrary_angle.py
@@ -10,17 +10,18 @@ import skimage as sk
 import open3d as o3d
 import random
 import time
+from sklearn.neighbors import NearestNeighbors
 
 def full_hough_to_ply(dir):
-  ellipse_ratio = 0.258
-  threshold = 40
+  ellipse_ratio = 0#.258
+  threshold = 20
 
   frame_names = os.listdir(dir)
   # sort the frames named 0.png, 1.png, ..., 100.png, ...
   # filter out every frame thats not an int
   frame_names = [frame for frame in frame_names if frame.split('.')[0].isdigit()]
   frame_names = sorted(frame_names, key=lambda x: int(x.split('.')[0]))
-  frame_names = frame_names[::4]
+  frame_names = frame_names[::8]
   frames = [cv2.imread(os.path.join(dir, frame), 0) for frame in frame_names if frame.endswith('.png')]
   theta_max = len(frames)
 
@@ -85,8 +86,8 @@ def full_hough_to_ply(dir):
 
 
     # save the hough spaces to a file
-    np.save("hough_spaces.npy", hough_spaces)
-    return  
+    #np.save("hough_spaces.npy", hough_spaces)
+    #return  
 
     #hough_spaces = post_process_hough(hough_spaces)
     with tqdm(total=hough_spaces.shape[0], desc="Post processing hough spaces") as pbar:
@@ -134,7 +135,7 @@ def full_hough_to_ply(dir):
     colors *= 255
     colors = colors.astype(np.uint8)
 
-    
+    """
     filename ="test_arbitrary_angle.ply"
 
     # write the points to a ply file
@@ -152,7 +153,7 @@ def full_hough_to_ply(dir):
       for i, point in enumerate(points):
         file.write(str(float(point[0])) + " " + str(float(point[1])) + " " + str(float(point[2])) + " " + str(colors[i][0]) + " " + str(colors[i][1]) + " " + str(colors[i][2]) + "\n")
     
-    
+    """
     o3d.visualization.draw_geometries([pcd])
 
     
@@ -457,6 +458,9 @@ def post_process_hough(hough_space):
 #@jit(nopython=True, debug=True)
 def post_process_hough_3d(hough_spaces):
 
+  # normalize
+  #hough_spaces = hough_spaces / np.max(hough_spaces)
+
   print("Removing low frequencies...")
 
   start = time.time()
@@ -482,16 +486,20 @@ def post_process_hough_3d(hough_spaces):
   mask_area = (x - center[0]) ** 2 + (y - center[1]) ** 2 + (z - center[2]) ** 2 <= radius**2
   mask[mask_area] = 1
   fshift = hough_spaces_fourier * mask
+  del mask, mask_area, hough_spaces_fourier
 
   f_ishift = np.fft.ifftshift(fshift)
+  del fshift
   hough_spaces = hough_spaces - np.abs(np.fft.ifftn(f_ishift))
+  del f_ishift
+
 
   # Weigh the matrix with weights e^(-0.001)*[1,2,...,A_max] along the amplitude axis
   print("Weighing the matrix...")
   for amplitude in range(1, hough_spaces.shape[1]):
     hough_spaces[:, amplitude, :] = hough_spaces[:, amplitude, :] * np.exp(-0.001 * amplitude)
 
-  return hough_spaces
+  #return hough_spaces
 
   hough_mask = hough_spaces.flatten()
   # normalize the mask
@@ -677,16 +685,407 @@ def hough_helper_sausage(grad_mag, hough_spaces, y, ellipse_ratio, threshold, ra
 
   return hough_spaces
   
+import copy
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.206, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    print("Transformation: " + str(transformation))
+    source_temp.transform(transformation)
+    coord_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    o3d.visualization.draw_geometries([source_temp, target_temp, coord_frame],
+                                  zoom=0.5,
+                                  front=[0.9288, -0.2951, -0.2242],
+                                  lookat=[0, 1, 1],
+                                  up=[0, 0, 1])
+
+
+def estimate_transformation(source, target, iterations=10):
+  # the source and target are hough spaces of shape (theta, a, z)
+
+  # normalize the source and target
+  source = source / np.max(source)
+  target = target / np.max(target)
+
+  print("Source shape: " + str(source.shape))
+  print("Target shape: " + str(target.shape))
+
+  source_points = np.argwhere(source > 0.2)
+  target_points = np.argwhere(target > 0.2)
+
+  theta_max = source.shape[0]
+
+  # plot the source and target points in 3d
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  ax.scatter(source_points[:, 0], source_points[:, 1], source_points[:, 2], c='r', label='Source')
+  ax.scatter(target_points[:, 0], target_points[:, 1], target_points[:, 2], c='b', label='Target')
+  ax.legend()
+  plt.show()
+
+  source_points = estimate_transformation_helper(source_points, target_points, theta_max, iterations=iterations)
+
+
   
+    
+
+  # plot the source and target points and the source points after transformation
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  ax.scatter(source_points[:, 0], source_points[:, 1], source_points[:, 2], c='r', label='Source')
+  ax.scatter(target_points[:, 0], target_points[:, 1], target_points[:, 2], c='b', label='Target')
+  ax.legend()
+  plt.show()
+
+@jit(nopython=True)
+def find_corresponding_points(source_points, target_points, theta_max):
+  distances = []
+  indices = []
+  for point in source_points:
+        nearest = None
+        min_dist = np.inf
+        for i, target_point in enumerate(target_points):
+          theta_diff = min((point[0] - target_point[0])**2, (point[0] - target_point[0] + theta_max)**2, (point[0] - target_point[0] - theta_max)**2)
+          a_diff = (point[1] - target_point[1])**2
+          z_diff = (point[2] - target_point[2])**2
+          dist = np.sqrt(theta_diff + a_diff + z_diff)
+          if dist < min_dist:
+            min_dist = dist
+            nearest = i
+        distances.append(min_dist)
+        indices.append(nearest)
+  return distances, indices
+  
+
+def estimate_transformation_helper(source_points, target_points, theta_max,  iterations=10):
+
+  for _ in range(iterations):
+    distances = []
+    indices = []
+
+    # find corresponding points
+    # for each point in source find the nearest point in target
+    # use the nearest neighbor algorithm
+    distances, indices = find_corresponding_points(source_points, target_points, theta_max)
+    
+    #with tqdm(total=len(source_points), desc="Finding corresponding points") as pbar:
+    #  for point in source_points:
+    #    nearest = None
+    #    min_dist = np.inf
+    #    for i, target_point in enumerate(target_points):
+    #      theta_diff = min((point[0] - target_point[0])**2, (point[0] - target_point[0] + theta_max)**2, (point[0] - target_point[0] - theta_max)**2)
+    #      a_diff = (point[1] - target_point[1])**2
+    #      z_diff = (point[2] - target_point[2])**2
+    #      dist = np.sqrt(theta_diff + a_diff + z_diff)
+    #      if dist < min_dist:
+    #        min_dist = dist
+    #        nearest = i
+    #    distances.append(min_dist)
+    #    indices.append(nearest)
+    #    #corresponding_points.append([point, nearest])
+    #    pbar.update(1)
+
+    #nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(target_points)
+    #distances, indices = nbrs.kneighbors(source_points)
+    #weights = np.array([source[source_points[i][0], source_points[i][1], source_points[i][2]] + target[target_points[indices[i][0]][0], target_points[indices[i][0]][1], target_points[indices[i][0]][2]] for i in range(len(source_points))])
+    #weights = weights / np.max(weights)
+    #scale = np.average(np.array([source_points[i] / target_points[indices[i]] for i in range(len(source_points))]), axis=0)#, weights=weights)
+    scale = np.mean(np.array([target_points[indices[i]] / source_points[i] for i in range(len(source_points))]), axis=0)#, weights=weights)
+    print(f"Scale: {scale}")
+    source_points[:, 1] = source_points[:, 1] * scale[1]  # scale in y direction
+    source_points[:, 2] = source_points[:, 2] * scale[2]  # scale in z direction
+
+    # translation (not in y direction)
+    #translation = np.average(np.array([source_points[i] - target_points[indices[i]] for i in range(len(source_points))]), axis=0)#, weights=weights)
+    translation = np.mean(np.array([target_points[indices[i]] - source_points[i] for i in range(len(source_points))]), axis=0)#, weights=weights)
+
+    translation_theta = np.mean(np.array([target_points[indices[i]][0] - source_points[i][0] for i in range(len(source_points))]), axis=0)
+    translation_theta = theta_max - translation_theta if np.abs(theta_max - translation_theta) < translation_theta else translation_theta
+    translation_theta = theta_max + translation_theta if np.abs(theta_max + translation_theta) < translation_theta else translation_theta 
+    print(f"Translation: {translation}")
+    source_points[:, 0] = (source_points[:, 0] + translation[0]) % theta_max
+    source_points[:, 1] = source_points[:, 1] + translation[1]
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(source_points[:, 0], source_points[:, 1], source_points[:, 2], c='r', label='Source')
+    ax.scatter(target_points[:, 0], target_points[:, 1], target_points[:, 2], c='b', label='Target')
+    #for i in range(len(source_points)):
+    #  ax.plot([source_points[i, 0], target_points[indices[i], 0]], [source_points[i, 1], target_points[indices[i], 1]], [source_points[i, 2], target_points[indices[i], 2]], c='g')
+    ax.legend()
+    plt.show()
+
+  return source_points
+    
+
+
+
+
+
+
+def estimate_transformation_test(source, target, iterations=10):
+
+  corresponding_points = []
+
+  # find corresponding points
+  # for each point in source find the nearest point in target
+  # use the nearest neighbor algorithm
+  for point in source:
+    nearest = (0, 0)
+    min_dist = 100000
+    for target_point in target:
+      #print(f"Point: {point}, Target point: {target_point}")
+      dist = np.sqrt((point[0] - target_point[0]) ** 2 + (point[1] - target_point[1]) ** 2)
+      if dist < min_dist:
+        min_dist = dist
+        nearest = target_point
+    corresponding_points.append([point, nearest])
+
+  # use the corresponding points to estimate the transformation
+  # scale
+  scale = np.mean(np.array([point[1] / point[0] for point in corresponding_points]), axis=0)
+  # multiply the source points with the scale
+  corresponding_points = np.array([point[0] * scale for point in corresponding_points])
+
+  # translation
+  translation = np.mean(np.array([(point[1] - point[0]) / 2 for point in corresponding_points]), axis=0)
+  #print(f"Translation: {translation}")
+
+  
+  #print(f"Scale: {scale}")
+
+  # use the translation and scale on the source points
+  source_transformed = np.array([point[0] * scale + translation for point in corresponding_points])
+
+  # plot the source and target points and the source points after transformation
+  if iterations == 0:
+    #plt.scatter(source_transformed[:, 0], source_transformed[:, 1], c='r', label='Source transformed')
+    #plt.scatter([point[1][0] for point in corresponding_points], [point[1][1] for point in corresponding_points], c='b', label='Target')
+    #plt.scatter([point[0][0] for point in corresponding_points], [point[0][1] for point in corresponding_points], c='g', label='Source')
+    #plt.legend()
+    #plt.show()
+    return source_transformed
+  
+
+  # recursivley iterate
+  estimate_transformation(source_transformed, target, iterations - 1)
 
 
 
 def test_hough():
+  
+  # load the hough spaces from a file
+  hough_spaces1 = np.load("hough_spaces.npy")
+  #hough_spaces1 = hough_spaces1[::2]
+  hough_spaces2 = np.load("hough_spaces_60d_orth_voronoi_1440.npy")
+  hough_spaces2 = hough_spaces2[::16]
+  print(hough_spaces1.shape)
+  print(hough_spaces2.shape)
+
+  hough_spaces1 = post_process_hough_3d(hough_spaces1)
+  hough_spaces2 = post_process_hough_3d(hough_spaces2)
+
+  estimate_transformation(hough_spaces1, hough_spaces2, iterations=10)
+
+  return
+
+  hough_spaces1 = post_process_hough_3d(hough_spaces1)
+  hough_spaces2 = post_process_hough_3d(hough_spaces2)
+
+  for i in range(34, hough_spaces1.shape[0], 10):
+    extremas1 = np.argwhere(hough_spaces1[i] > 0)
+    extremas2 = np.argwhere(hough_spaces2[i] > 0)
+    if len(extremas1) > 0 and len(extremas2) > 0:
+      source_transformed = estimate_transformation(extremas1, extremas2, iterations=10)
+      if source_transformed is not None:
+        # plot the source and target points and the source points after transformation
+        plt.scatter(source_transformed[:, 0], source_transformed[:, 1], c='r', label='Source transformed')
+        plt.scatter(extremas2[:, 0], extremas2[:, 1], c='b', label='Target')
+        plt.scatter(extremas1[:, 0], extremas1[:, 1], c='g', label='Source')
+        plt.legend()
+  return
+  
+
+  #extrema1 = np.load(os.path.join("..", "post_processed_mirror_60d.npy"))
+  #extrema2 = np.load(os.path.join("..", "post_processed_mirror_90d.npy"))
+
+  #hough_spaces1 = np.zeros((1440, 960, 1080))
+  #hough_spaces2 = np.zeros((1440, 960, 1080))
+
+  #hough_spaces1[extrema1[:, 0], extrema1[:, 1], extrema1[:, 2]] = 1
+  #hough_spaces2[extrema2[:, 0], extrema2[:, 1], extrema2[:, 2]] = 1
+
+  # source and target are images with possibly different sizes
+  extrema_source = np.argwhere(hough_spaces1[100] > 0)
+  #extrema_target = np.argwhere(target > 0)
+  extrema_target = np.argwhere(hough_spaces2[100] > 0)
+  # translate the target points by 100
+  #extrema_target = extrema_target + 20
+
+  estimate_transformation(extrema_source, extrema_target, iterations=10)
+  return
+
+  points1 = []
+  points2 = []
+
+  for theta_base, a, z  in extrema1:
+      theta = (theta_base / 1440) * 2 * np.pi
+      # get the x and y values
+      x = a * np.cos(theta)
+      y = a * np.sin(theta)
+      points1.append([x, y, z])
+
+  for theta_base, a, z  in extrema2:
+      theta = (theta_base / 1440) * 2 * np.pi
+      # get the x and y values
+      x = a * np.cos(theta)
+      y = a * np.sin(theta)
+      points2.append([x, y, z])
+
+  # use the hough spaces as source and target point clouds
+  source = o3d.geometry.PointCloud()
+  source.points = o3d.utility.Vector3dVector(points1)
+
+  #target = o3d.geometry.PointCloud()
+  #target.points = o3d.utility.Vector3dVector(points2)
+  target = o3d.io.read_point_cloud("point_cloud_1080_bam.ply")
+  # turn by 180 degrees
+  target.rotate([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
+
+
+
+  # load two point clouds
+  #source = o3d.io.read_point_cloud("75d_honey_thresholded.ply")
+  #source.scale(0.82, center=source.get_center())
+  #target = o3d.io.read_point_cloud("60d_orthographic_voronoi_1440.ply")
+
+  # iterative closest point algorithm
+  
+  trans_init = np.asarray([[1.4, 0, 0, 0],
+                            [0, 1.4, 0, 0],
+                            [0, 0, 1.4, 0],
+                            [0, 0, 0, 1]])
+  print("Initial alignment")
+  draw_registration_result(source, target, trans_init)
+  print("Apply point-to-point ICP")
+
+  estimation_method = o3d.pipelines.registration.TransformationEstimationPointToPoint()
+  estimation_method.with_scaling = True
+
+  convergence_criteria = o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=50)
+
+  reg_p2p = o3d.pipelines.registration.registration_icp(
+      source, target, 50, estimation_method=estimation_method
+  )
+  print(reg_p2p)
+  draw_registration_result(source, target, reg_p2p.transformation)
+
+
+
+
+  """
+  # load the hough spaces from a file
+  hough_spaces1 = np.load("hough_spaces_75d_honey_360.npy")
+  hough_spaces1 = hough_spaces1[::4]
+  hough_spaces2 = np.load("hough_spaces_60d_orth_voronoi_1440.npy")
+  hough_spaces2 = hough_spaces2[::16]
+
+  print(hough_spaces1.shape)
+  print(hough_spaces2.shape)
+
+  hough_spaces1 = post_process_hough_3d(hough_spaces1)
+  hough_spaces2 = post_process_hough_3d(hough_spaces2)
+
+  # choose 3 random hough spaces
+  rand1 = random.randint(0, hough_spaces1.shape[0] - 1)
+  rand2 = random.randint(0, hough_spaces1.shape[0] - 1)
+  rand3 = random.randint(0, hough_spaces1.shape[0] - 1)
+
+  hough_space1_1 = hough_spaces1[rand1]
+  hough_space1_2 = hough_spaces1[rand2]
+  hough_space1_3 = hough_spaces1[rand3]
+
+  hough_space2_1 = hough_spaces2[rand1]
+  hough_space2_2 = hough_spaces2[rand2]
+  hough_space2_3 = hough_spaces2[rand3]
+
+  shape1 = np.max([hough_space1_1.shape[0], hough_space2_1.shape[0]])
+  shape2 = np.max([hough_space1_1.shape[1], hough_space2_1.shape[1]])
+
+  shape = (shape1, shape2)
+  print(shape)
+
+  shape1 = hough_space1_1.shape
+  shape2 = hough_space2_1.shape
+
+  hough_diff1 = np.zeros((shape[0], shape[1], 3))
+  hough_diff2 = np.zeros((shape[0], shape[1], 3))
+  hough_diff3 = np.zeros((shape[0], shape[1], 3))
+
+  hough_diff1[:shape1[0], :shape1[1], :] = np.repeat(hough_space1_1[:, :, np.newaxis], 3, axis=2)
+  hough_diff2[:shape1[0], :shape1[1], :] = np.repeat(hough_space1_2[:, :, np.newaxis], 3, axis=2)
+  hough_diff3[:shape1[0], :shape1[1], :] = np.repeat(hough_space1_3[:, :, np.newaxis], 3, axis=2)
+
+  hough_diff1[:hough_space2_1.shape[0], :hough_space2_1.shape[1], 0] -= hough_space2_1
+  hough_diff2[:hough_space2_2.shape[0], :hough_space2_2.shape[1], 0] -= hough_space2_2
+  hough_diff3[:hough_space2_3.shape[0], :hough_space2_3.shape[1], 0] -= hough_space2_3
+
+  hough_diff1[:hough_space2_1.shape[0], :hough_space2_1.shape[1], 2] = hough_space2_1
+  hough_diff2[:hough_space2_2.shape[0], :hough_space2_2.shape[1], 2] = hough_space2_2
+  hough_diff3[:hough_space2_3.shape[0], :hough_space2_3.shape[1], 2] = hough_space2_3
+
+  hough_diff1[:,:, 0] = 0#np.abs(hough_diff1[:,:, 0])
+  hough_diff2[:,:, 0] = 0#np.abs(hough_diff2[:,:, 0])
+  hough_diff3[:,:, 0] = 0#np.abs(hough_diff3[:,:, 0])
+
+
+
+
+  # overlay them
+  fig, axs = plt.subplots(2, 3)
+  axs[0, 0].imshow(hough_space1_1, cmap='inferno')
+  axs[0, 1].imshow(hough_space1_2, cmap='inferno')
+  axs[0, 2].imshow(hough_space1_3, cmap='inferno')
+
+  axs[1, 0].imshow(hough_space2_1, cmap='inferno')
+  axs[1, 1].imshow(hough_space2_2, cmap='inferno')
+  axs[1, 2].imshow(hough_space2_3, cmap='inferno')
+
+  # title is the frame number
+  axs[0, 0].set_title(f"Frame {rand1}")
+  axs[0, 1].set_title(f"Frame {rand2}")
+  axs[0, 2].set_title(f"Frame {rand3}")
+
+  plt.show()
+
+  plt.imshow(hough_diff1, interpolation='none')
+  plt.colorbar()
+  plt.show()
+
+  plt.imshow(hough_diff2, interpolation='none')
+  plt.colorbar()
+  plt.show()
+
+  plt.imshow(hough_diff3, interpolation='none')
+  plt.colorbar()
+  plt.show()
+  """
+
+
+
+
+def test_hough2():
   # load the hough spaces from a file
   hough_spaces = np.load("hough_spaces_60d_orth_voronoi_1440.npy")
+  #hough_spaces = np.load("hough_spaces_sausage.npy")
 
   # only take every 5th frame
-  #hough_spaces = hough_spaces[::4]
+  hough_spaces = hough_spaces[::4]
+
+  # reorder from z, a, theta to theta, a, z
+  #hough_spaces = np.moveaxis(hough_spaces, 0, 2) 
+  #hough_spaces = np.moveaxis(hough_spaces, 0, 1)
+
   print(hough_spaces.shape)
 
 
@@ -823,6 +1222,7 @@ def main():
   #dir = os.path.join("..", "scratch", "60d_100mm_10mm_voronoi", "rows")
   #dir = os.path.join("..", "scratch", "circle_moving")
   dir = os.path.join("..", "scratch", "75d_100mm_10mm_honey")
+  dir = os.path.join("..", "scratch", "full_1440")
   full_hough_to_ply(dir)
 
 if __name__ == "__main__":
